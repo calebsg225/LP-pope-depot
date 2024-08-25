64a3e492987b931c485055548220576e8ea24012
caleb <calebsg2253284+git@gmail.com>
2024-08-24 23:07:08 -0400

    add hero image, create main section (mobile)

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import thePopeDepotLogo from './assets/Pope-Depot-Logo.jpg';
+import popeHeroImage from './assets/Pope-Depot-Hero-Image_crop.png';
 import CallToAction from './components/CallToAction';
 
 function App() {
@@ -6,10 +7,21 @@ function App() {
   return (
     <>
       <header>
-        <img src={thePopeDepotLogo} alt="the_pope-depot-logo" />
+        <img src={thePopeDepotLogo} alt="the_pope_depot_logo" />
         <CallToAction text='Shop Today'/>
+        <div>
+          <p>#1 Pope Improvement Retailer</p>
+        </div>
       </header>
       <main>
+        <section className='action-container'>
+          <h1>Attire For <span>Every</span>one</h1>
+          <h2>Mitres, Crucifixes, Robes, and more...</h2>
+          <CallToAction text='Shop Today'/>
+        </section>
+        <section className='hero-container'>
+          <img src={popeHeroImage} alt="hero_image" />
+        </section>
       </main>
       <footer></footer>
     </>
